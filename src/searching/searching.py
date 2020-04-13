348cbe55428003ae91459def9ccb326b96b8f16e
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2020-04-13 18:41:33 -0400

    searching.py tests passing mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,30 +1,60 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
+    # TO-DO: add missing code
+    answer = -1
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            answer = i
+        else:
+            pass
 
-   return -1   # not found
+    return answer  # not found
 
 
-# STRETCH: write an iterative implementation of Binary Search 
+# STRETCH: write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    answer = -1
+    unanswered = True
+    if len(arr) == 0:
+        return answer  # array empty
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    low = 0
+    high = len(arr) - 1
 
-  # TO-DO: add missing code
+    # TO-DO: add missing code
+    while unanswered:
+        split = (high - low) // 2
+        if target == arr[split]:
+            answer = split
+            unanswered = False
+        elif target < arr[split]:
+            high = split
+        else:
+            low = split + 1
 
-  return -1 # not found
+    return answer  # not found
 
 
-# STRETCH: write a recursive implementation of Binary Search 
+# STRETCH: write a recursive implementation of Binary Search
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
+    low = low
+    high = high
+    middle = (low + high) // 2
+    answer = -1
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    if len(arr) == 0:
+        return answer  # array empty
+    # TO-DO: add missing if/else statements, recursive calls
+
+    # Base case
+    if arr[middle] == target:
+        answer = middle
+        return answer
+
+    # Test if target is lower or higher than middle
+    if arr[middle] > target:
+        high = middle
+    else:
+        low = middle + 1
+
+    return binary_search_recursive(arr, target, low, high)
